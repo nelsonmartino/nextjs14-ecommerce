6590e4e7ca7c7a7f4a6ea7ec58606ca0cac56c20
nelsonmartino <nelsonmartino@gmail.com>
2024-01-25 14:50:26 -0300

    first commit

---
+++ components/header/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            NextJs 14 Ecommerce
+          </Link>
+          <ul className="flex">
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/cart">
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/signin">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
